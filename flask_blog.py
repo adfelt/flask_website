b2fb39855a1c46ab1a116d2b6f6060820bd69ed4
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2024-01-06 20:43:43 -0800

    Feat: First commit

---
+++ flask_blog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author':'Corey Schafer',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'January 5th, 2024'
+    },
+    {
+        'author':'John Doe',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'January 6th, 2024'
+    },
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
