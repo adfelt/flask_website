9dfc0c4035a1bc66cdf6ff5a07b5f8547afa0c47
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2024-01-29 22:30:44 -0800

    Feat: Added account update features

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import Users
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -30,3 +31,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    
+    
+    
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(), Length(min=2, max=30)])
+    email = StringField('Email',
+                            validators=[DataRequired(), Email()])
+    
+    submit = SubmitField('Update')
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = Users.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different name')
+        
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = Users.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('That email already registered. Please choose a different name')
