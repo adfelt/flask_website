4c91f5fa941d84ccfa88a8adc11cf560c0b4383a
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2024-01-31 11:51:54 -0800

    Feat: Allowed user to change profile picture

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import Users
@@ -38,7 +39,7 @@ class UpdateAccountForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(), Length(min=2, max=30)])
     email = StringField('Email',
                             validators=[DataRequired(), Email()])
-    
+    picture = FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png'])])
     submit = SubmitField('Update')
     
     def validate_username(self, username):
