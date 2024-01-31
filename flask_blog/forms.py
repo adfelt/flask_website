f3875dceb1ae46c21c51c2b5c02c679eb358290a
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2024-01-31 13:16:27 -0800

    Feat: Added a post page and validtion

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import Users
 from flask_login import current_user
@@ -53,3 +53,9 @@ class UpdateAccountForm(FlaskForm):
             email = Users.query.filter_by(email=email.data).first()
             if email:
                 raise ValidationError('That email already registered. Please choose a different name')
+            
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+    
