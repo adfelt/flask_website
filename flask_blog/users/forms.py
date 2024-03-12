f34a412ee74d411ca81980ae842cea30fb385cd7
Andrew Felt <75708105+adfelt@users.noreply.github.com>
2024-03-11 23:40:04 -0700

    build: Restructured app using flask blueprints

--- flask_blog/users/forms.py
+++ flask_blog/users/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from flask_wtf.file import FileAllowed, FileField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flask_blog.models import Users
 from flask_login import current_user
+from flask_blog.models import Users
 
 
 class RegistrationForm(FlaskForm):
@@ -54,13 +54,6 @@ class UpdateAccountForm(FlaskForm):
             if email:
                 raise ValidationError('That email already registered. Please choose a different name')
             
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-    
-    
-    
 class RequestResetForm(FlaskForm):
     email = StringField('Email',
                             validators=[DataRequired(), Email()])
@@ -77,4 +70,3 @@ class ResetPasswordForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                         validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Reset Password')
-    
